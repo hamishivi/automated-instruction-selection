ff6c0e6ed99a21ef40e2a6c57f173412b6982d54
Nanami18 <nanami17@cs.washington.edu>
2024-03-05 11:35:26 -0800

    adapting hamish code for new tasks

--- minimal_multitask/data.py
+++ minimal_multitask/data.py
@@ -5,7 +5,13 @@ import json
 from datasets import load_dataset, Dataset
 from minimal_multitask.eval.run_mmlu_eval import construct_prompts
 from minimal_multitask.eval.run_alpaca_eval import create_prompt_with_tulu_chat_format
-
+from minimal_multitask.gsm_exemplars import EXAMPLARS as GSM_EXAMPLARS
+import glob
+import os
+import tqdm
+import random
+import torch
+import re
 
 class TestDataset:
     def __init__(self, tokenizer) -> None:
@@ -17,22 +23,21 @@ class TestDataset:
 # turns sample into something we can feed into a model
 def construct_test_sample(tokenizer, sample, max_length=1024):
     prompt, label = sample['prompts'], sample['labels']
-    # note no space between prompt and label
-    # have to put max length! sometimes gpt-4 will generate a lot of text
-    inputs = tokenizer(prompt + label + tokenizer.eos_token, return_tensors='pt', max_length=max_length, truncation=True)
-    input_len = len(tokenizer(prompt).input_ids)
-    labels = inputs['input_ids'][0].clone()
-    labels[:input_len] = -100
+    # tokenizer label and prompt independently, then concatenate.
+    # this is so we match the test format (tokenize the prompt and generate)
+    inputs = tokenizer(prompt, return_tensors='pt', max_length=max_length, truncation=True)
+    labels = tokenizer(label + tokenizer.eos_token, return_tensors='pt', max_length=max_length, truncation=True, add_special_tokens=False)
     return {
-        'input_ids': inputs['input_ids'][0],
-        'attention_mask': inputs['attention_mask'][0],
-        'labels': labels
+        'input_ids': torch.cat([inputs['input_ids'][0], labels['input_ids'][0]], dim=0),
+        'attention_mask': torch.cat([inputs['attention_mask'][0], labels['attention_mask'][0]], dim=0),
+        'labels': torch.cat([torch.ones_like(inputs['input_ids'][0]) * -100, labels['input_ids'][0]], dim=0)
     }
 
 
 class MMLU(TestDataset):
+    shots = 0
     def get_all_test_prompts(self, num_samples=500, seed=42):
-        prompts, labels = construct_prompts(self.tokenizer, use_chat_format=True)
+        prompts, labels = construct_prompts(self.tokenizer, use_chat_format=True, ntrain=self.shots)
         test_dataset = Dataset.from_dict({'prompts': prompts, 'labels': labels})
         make_test_sample = lambda x: construct_test_sample(self.tokenizer, x, max_length=1024)
         test_dataset = test_dataset.map(make_test_sample, load_from_cache_file=False)
@@ -72,8 +77,278 @@ class SquadEval(TestDataset):
         test_dataset = test_dataset.shuffle(seed=seed).select(range(num_samples))
         return test_dataset
 
+class BBHEval(TestDataset):
+    subsets = ['boolean_expressions', 'causal_judgement', 'date_understanding', 'disambiguation_qa', 'dyck_languages', 'formal_fallacies', 'geometric_shapes', 'hyperbaton', 'logical_deduction_five_objects', 'logical_deduction_seven_objects', 'logical_deduction_three_objects', 'movie_recommendation', 'multistep_arithmetic_two', 'navigate', 'object_counting', 'penguins_in_a_table', 'reasoning_about_colored_objects', 'ruin_names', 'salient_translation_error_detection', 'snarks', 'sports_understanding', 'temporal_sequences', 'tracking_shuffled_objects_five_objects', 'tracking_shuffled_objects_seven_objects', 'tracking_shuffled_objects_three_objects', 'web_of_lies', 'word_sorting']
+    cot = False
+    data_dir = "/net/nfs.cirrascale/allennlp/hamishi/open-instruct/data/eval/bbh"
+    max_num_examples_per_task = 10
+
+    def get_all_test_prompts(self, num_samples=500, seed=42):
+        random.seed(42)
+        all_tasks = {}
+        tokenizer = self.tokenizer
+        task_files = glob.glob(os.path.join(self.data_dir, "bbh", "*.json"))
+        for task_file in tqdm.tqdm(task_files, desc="Loading tasks"):
+            with open(task_file, "r") as f:
+                task_name = os.path.basename(task_file).split(".")[0]
+                all_tasks[task_name] = json.load(f)["examples"]
+                if self.max_num_examples_per_task:
+                    all_tasks[task_name] = random.sample(all_tasks[task_name], self.max_num_examples_per_task)
+        all_prompts = {}
+        cot_prompt_files = glob.glob(os.path.join(self.data_dir, "cot-prompts", "*.txt"))
+        for cot_prompt_file in tqdm.tqdm(cot_prompt_files, desc="Loading prompts"):
+            with open(cot_prompt_file, "r") as f:
+                task_name = os.path.basename(cot_prompt_file).split(".")[0]
+                task_prompt = "".join(f.readlines()[2:])
+                if not self.cot:
+                    prompt_fields = task_prompt.split("\n\n")
+                    new_prompt_fields = []
+                    for prompt_field in prompt_fields:
+                        if prompt_field.startswith("Q:"):
+                            assert "So the answer is" in prompt_field, f"`So the answer is` not found in prompt field of {task_name}.txt."
+                            assert "\nA:" in prompt_field, "`\nA:` not found in prompt field."
+                            answer = prompt_field.split("So the answer is")[-1].strip()
+                            question = prompt_field.split("\nA:")[0].strip()
+                            new_prompt_fields.append(question + "\nA: " + answer)
+                        else:
+                            new_prompt_fields.append(prompt_field)
+                    task_prompt = "\n\n".join(new_prompt_fields)
+                all_prompts[task_name] = task_prompt
+        prompts = []
+        labels = []
+        for task_name in tqdm.tqdm(all_tasks.keys(), desc="Evaluating"):
+            task_examples = all_tasks[task_name]
+            task_prompt = all_prompts[task_name]
+            for example in task_examples:
+                prompt = task_prompt.strip() + "\n\nQ: " + example["input"]
+                messages = [{"role": "user", "content": prompt}]
+                prompt = create_prompt_with_tulu_chat_format(messages, self.tokenizer, add_bos=False)
+                prompt += "A:" if prompt[-1] in ["\n", " "] else " A:"
+                prompts.append(prompt)
+                labels.append(example["target"])
+        test_dataset = Dataset.from_dict({'prompts': prompts, 'labels': labels})
+        test_dataset = test_dataset.map(lambda x: construct_test_sample(self.tokenizer, x, max_length=1024))
+        test_dataset.set_format(type='torch', columns=['input_ids', 'attention_mask', 'labels'])
+        test_dataset = test_dataset.shuffle(seed=seed).select(range(min(num_samples, len(test_dataset))))
+        return test_dataset
+
+class TydiqaEval(TestDataset):
+    data_dir = "/net/nfs.cirrascale/allennlp/hamishi/open-instruct/data/eval/tydiqa"
+    max_num_examples_per_lang = 10
+    n_shot = 1
+    encoding_templates_with_context = {
+        "english": ("Answer the following question based on the information in the given passage.", "Passage:", "Question:", "Answer:"),
+        "arabic": ("أجب على السؤال التالي بناءً على المعلومات في المقطع المعطى.", "المقطع:", "السؤال:", "الإجابة:"),
+        "bengali": ("প্রদত্ত অধ্যায়ের তথ্যের উপর ভিত্তি করে নিম্নলিখিত প্রশ্নের উত্তর দিন।", "অধ্যায়:", "প্রশ্ন:", "উত্তর:"),
+        "finnish": ("Vastaa seuraavaan kysymykseen annetun kappaleen tiedon perusteella.", "Kappale:", "Kysymys:", "Vastaus:"),
+        "indonesian": ("Jawab pertanyaan berikut berdasarkan informasi di bagian yang diberikan.", "Bagian:", "Pertanyaan:", "Jawaban:"),
+        "korean": ("주어진 문단의 정보에 기반하여 다음 질문에 답하십시오.", "문단:", "질문:", "답변:"),
+        "russian": ("Ответьте на следующий вопрос на основе информации в данном отрывке.", "Отрывок:", "Вопрос:", "Ответ:"),
+        "swahili": ("Jibu swali lifuatalo kulingana na habari kwenye kifungu kilichotolewa.", "Kifungu:", "Swali:", "Jibu:"),
+        "telugu": ("ఇచ్చిన పేరాలోని సమాచారం ఆధారంగా కింది ప్రశ్నకు సమాధానం ఇవ్వండి.", "పేరా:", "ప్రశ్న:", "సమాధానం:")
+    }
+    encoding_templates_without_context = {
+        "english": ("Answer the following question.", "Question:", "Answer:"),
+        "arabic": ("أجب على السؤال التالي.", "السؤال:", "الإجابة:"),
+        "bengali": ("নিম্নলিখিত প্রশ্নের উত্তর দিন।", "প্রশ্ন:", "উত্তর:"),
+        "finnish": ("Vastaa seuraavaan kysymykseen.", "Kysymys:", "Vastaus:"),
+        "indonesian": ("Jawab pertanyaan berikut.", "Pertanyaan:", "Jawaban:"),
+        "korean": ("다음 질문에 답하십시오.", "질문:", "답변:"),
+        "russian": ("Ответьте на следующий вопрос.", "Вопрос:", "Ответ:"),
+        "swahili": ("Jibu swali lifuatalo.", "Swali:", "Jibu:"),
+        "telugu": ("క్రింది ప్రశ్నకు సమాధానం ఇవ్వండి.", "ప్రశ్న:", "సమాధానం:")
+    }
+    max_context_length = 1024
+    no_context = False
+    def get_all_test_prompts(self, num_samples=500, seed=42):
+        test_data = []
+        tokenizer = self.tokenizer
+        with open(os.path.join(self.data_dir, "tydiqa-goldp-v1.1-dev.json")) as fin:
+            dev_data = json.load(fin)
+            for article in dev_data["data"]:
+                for paragraph in article["paragraphs"]:
+                    for qa in paragraph["qas"]:
+                        example = {
+                            "id": qa["id"], 
+                            "lang": qa["id"].split("-")[0],
+                            "context": paragraph["context"],
+                            "question": qa["question"],
+                            "answers": qa["answers"]
+                        }
+                        test_data.append(example)
+        data_languages = sorted(list(set([example["lang"] for example in test_data]))) 
+        if self.max_num_examples_per_lang:
+            sampled_examples = []
+            for lang in data_languages:
+                examples_for_lang = [example for example in test_data if example["lang"] == lang]
+                if len(examples_for_lang) > self.max_num_examples_per_lang:
+                    examples_for_lang = random.sample(examples_for_lang, self.max_num_examples_per_lang)
+                sampled_examples += examples_for_lang
+            test_data = sampled_examples
+        
+        print(f"Loaded {len(test_data)} examples from {len(data_languages)} languages: {data_languages}")
+
+        if self.n_shot > 0:
+            train_data_for_langs = {lang: [] for lang in data_languages}
+            with open(os.path.join(self.data_dir, "tydiqa-goldp-v1.1-train.json")) as fin:
+                train_data = json.load(fin)
+                for article in train_data["data"]:
+                    for paragraph in article["paragraphs"]:
+                        for qa in paragraph["qas"]:
+                            lang = qa["id"].split("-")[0]
+                            if lang in data_languages:
+                                example = {
+                                    "id": qa["id"],
+                                    "lang": lang,
+                                    "context": paragraph["context"],
+                                    "question": qa["question"],
+                                    "answers": qa["answers"]
+                                }
+                                train_data_for_langs[lang].append(example)
+                for lang in data_languages:
+                    # sample n_shot examples from each language
+                    train_data_for_langs[lang] = random.sample(train_data_for_langs[lang], self.n_shot)
+            # assert that we have exactly n_shot examples for each language
+            assert all([len(train_data_for_langs[lang]) == self.n_shot for lang in data_languages])
+
+        # assert we have templates for all data languages
+        assert all([lang in self.encoding_templates_with_context.keys() for lang in data_languages])
+
+        if self.max_context_length:
+            for example in test_data:
+                tokenized_context = tokenizer.encode(example["context"])
+                if len(tokenized_context) > self.max_context_length:
+                    example["context"] = tokenizer.decode(tokenized_context[:self.max_context_length])
+            if self.n_shot > 0:
+                for lang in data_languages:
+                    for example in train_data_for_langs[lang]:
+                        tokenized_context = tokenizer.encode(example["context"])
+                        if len(tokenized_context) > self.max_context_length:
+                            example["context"] = tokenizer.decode(tokenized_context[:self.max_context_length])
+
+        prompts = []
+        labels = []
+        for example in test_data:
+            lang = example["lang"]
+
+            if self.no_context:
+                prompt, q_template, a_template = self.encoding_templates_without_context[lang]
+                p_template = ""
+            else:
+                prompt, p_template, q_template, a_template = self.encoding_templates_with_context[lang]
+
+            prompt += "\n\n"
+            
+            if self.n_shot > 0:
+                formatted_demo_examples = []
+                for train_example in train_data_for_langs[lang]:
+                    if self.no_context:
+                        formatted_demo_examples.append(
+                            q_template + " " + train_example["question"] + "\n" + a_template + " " + train_example["answers"][0]["text"]
+                        )
+                    else:
+                        formatted_demo_examples.append(
+                            p_template + " " + train_example["context"] + "\n" + q_template + " " + train_example["question"] + "\n" + a_template + " " + train_example["answers"][0]["text"]
+                        )
+                prompt += "\n\n".join(formatted_demo_examples) + "\n\n"
+            
+            if self.no_context:
+                prompt += q_template + " " + format(example["question"]) + "\n"
+            else:
+                prompt += p_template + " " + format(example["context"]) + "\n" + q_template + " " + format(example["question"]) + "\n"
+
+            messages = [{"role": "user", "content": prompt}]
+            prompt = create_prompt_with_tulu_chat_format(messages, self.tokenizer, add_bos=False)
+            prompt += a_template if prompt[-1] in ["\n", " "] else " " + a_template
+            prompts.append(prompt)
+            labels.append(example["answers"][0]['text'])
+        test_dataset = Dataset.from_dict({'prompts': prompts, 'labels': labels})
+        test_dataset = test_dataset.map(lambda x: construct_test_sample(self.tokenizer, x, max_length=1024))
+        test_dataset.set_format(type='torch', columns=['input_ids', 'attention_mask', 'labels'])
+        test_dataset = test_dataset.shuffle(seed=seed).select(range(min(num_samples, len(test_dataset))))
+        return test_dataset
+
+class GSM8kEval(TestDataset):
+    data_dir = "/net/nfs.cirrascale/allennlp/hamishi/open-instruct/data/eval/gsm"
+    n_shot = 8
+    cot = True
+    def get_all_test_prompts(self, num_samples=500, seed=42):
+        test_data = []
+        tokenizer = self.tokenizer
+        with open(os.path.join(self.data_dir, f"test.jsonl")) as fin:
+            for line in fin:
+                example = json.loads(line)
+                test_data.append({
+                    "question": example["question"],
+                    "answer": example["answer"].split("####")[1].strip()
+                })
+            
+        # some numbers are in the `x,xxx` format, and we want to remove the comma
+        for example in test_data:
+            example["answer"] = re.sub(r"(\d),(\d)", r"\1\2", example["answer"])
+            assert float(example["answer"]), f"answer is not a valid number: {example['answer']}"
+
+        if num_samples and len(test_data) > num_samples:
+            test_data = random.sample(test_data, num_samples)
+
+        global GSM_EXAMPLARS
+        if self.n_shot:
+            if len(GSM_EXAMPLARS) > self.n_shot:
+                GSM_EXAMPLARS = random.sample(GSM_EXAMPLARS, self.n_shot)
+            demonstrations = []
+            for example in GSM_EXAMPLARS:
+                if not self.cot:
+                    demonstrations.append(
+                        "Quesion: " + example["question"] + "\n" + "Answer: " + example["short_answer"]
+                    )
+                else:
+                    demonstrations.append(
+                        "Question: " + example["question"] + "\n" + "Answer: " + example["cot_answer"]
+                    )
+            prompt_prefix = "Answer the following questions.\n\n" + "\n\n".join(demonstrations) + "\n\n"
+        else:
+            prompt_prefix = "Answer the following question.\n\n"
+
+        prompts = []
+        labels = []
+        def apply_chat_format(example, tokenizer):
+            messages = [{"role": "user", "content": prompt_prefix + "Question: " + example["question"].strip()}]
+            prompt = create_prompt_with_tulu_chat_format(messages, tokenizer, add_bos=False)
+            prompt += "Answer:" if prompt[-1] in ["\n", " "] else " Answer:"
+            return prompt
+        for example in test_data:
+            prompts.append(apply_chat_format(example, tokenizer))
+            labels.append(example["answer"])
+        test_dataset = Dataset.from_dict({'prompts': prompts, 'labels': labels})
+        test_dataset = test_dataset.map(lambda x: construct_test_sample(self.tokenizer, x, max_length=1024))
+        test_dataset.set_format(type='torch', columns=['input_ids', 'attention_mask', 'labels'])
+        test_dataset = test_dataset.shuffle(seed=seed).select(range(min(num_samples, len(test_dataset))))
+        return test_dataset
+
+
+# todo: humaneval, truthfulqa mc.
+# these are maybe tricky since they dont have hard gold.
+
 DATASETS = {
     'mmlu': MMLU,
     'alpacafarm': AlpacaEval,
-    'squad': SquadEval
+    'squad': SquadEval,
+    'bbh': BBHEval,
+    'tydiqa': TydiqaEval,
+    'gsm8k': GSM8kEval
 }
+
+if __name__ == "__main__":
+    from transformers import AutoTokenizer
+    tokenizer = AutoTokenizer.from_pretrained('oobabooga/llama-tokenizer')
+    print("Test which dataset?")
+    dataset = input().strip()
+    dataset = DATASETS[dataset](tokenizer)
+    test_dataset = dataset.get_all_test_prompts()
+    print(test_dataset[0])
+    print(tokenizer.decode(test_dataset[0]['input_ids']))
+    print(tokenizer.decode(test_dataset[0]['labels'][(test_dataset[0]['labels'] == -100).sum():]))
+    print(test_dataset[1])
+    print(tokenizer.decode(test_dataset[1]['input_ids']))
+    print(tokenizer.decode(test_dataset[1]['labels'][(test_dataset[1]['labels'] == -100).sum():]))
+    print(len(test_dataset))
