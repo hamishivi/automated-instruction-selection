2b4136df356f650dfc9cc63a225c3de701f4acb5
Hamish Ivison <hamishi@allennlp-cirrascale-20.reviz.ai2.in>
2024-04-09 00:18:50 +0000

    report more numbers for squad

--- minimal_multitask/eval/squad/run_squad_eval.py
+++ minimal_multitask/eval/squad/run_squad_eval.py
@@ -6,6 +6,7 @@ from argparse import ArgumentParser
 import vllm
 from transformers import AutoTokenizer
 from minimal_multitask.eval.alpaca_eval.run_alpaca_eval import create_prompt_with_tulu_chat_format
+from minimal_multitask.eval.squad.squad_eval_1 import evaluate
 
 parser = ArgumentParser()
 parser.add_argument('--model_name', type=str, default='EleutherAI/pythia-70m')
@@ -17,23 +18,18 @@ args = parser.parse_args()
 tokenizer = AutoTokenizer.from_pretrained(args.model_name)
 
 squad_og = load_dataset('squad', split='validation')
-squad_inf_split_og = squad_og.shuffle(seed=42).select(range(500))
 
 # convert everything to tulu
 def convert_squad_sample(sample):
     prompt = sample['context'] + '\n\n' + sample['question']
     label = sample['answers']['text'][0]
     messages = [{"role": "user", "content": prompt}]
-    prompt = create_prompt_with_tulu_chat_format(messages, tokenizer, add_bos=False) + 'Answer: '
+    prompt = create_prompt_with_tulu_chat_format(messages, tokenizer, add_bos=False) # + 'Answer: '
     return {'prompt': prompt, 'label': label}
 
 squad = squad_og.map(convert_squad_sample, load_from_cache_file=False)
 squad.set_format(type='torch', columns=['prompt', 'label'])
 prompts = squad['prompt']
-
-squad_inf_split = squad_inf_split_og.map(convert_squad_sample, load_from_cache_file=False)
-squad_inf_split.set_format(type='torch', columns=['prompt', 'label'])
-inf_prompts = squad_inf_split['prompt']
 
 # predict over dataset
 model = vllm.LLM(
@@ -43,37 +39,24 @@ model = vllm.LLM(
     )
 sampling_params = vllm.SamplingParams(
     temperature=0,  # greedy decoding
-    max_tokens=50,  # I don't think anything should go over this?
+    max_tokens=2048,  # I don't think anything should go over this?
 )
 
 outputs = model.generate(prompts, sampling_params)
 outputs = [it.outputs[0].text.strip() for it in outputs]
 
-inf_outputs = model.generate(inf_prompts, sampling_params)
-inf_outputs = [it.outputs[0].text.strip() for it in inf_outputs]
-
 # calculate squad f1
-f1_scorer = evaluate.load("squad")
 references = [{'id': sample['id'], 'answers': sample['answers']} for sample in squad_og]
 outputs = [{'id': squad_og[i]['id'], 'prediction_text': output} for i, output in enumerate(outputs)]
-results = f1_scorer.compute(references=references, predictions=outputs)
-inf_references = [{'id': sample['id'], 'answers': sample['answers']} for sample in squad_inf_split_og]
-inf_outputs = [{'id': squad_inf_split_og[i]['id'], 'prediction_text': output} for i, output in enumerate(inf_outputs)]
-inf_results = f1_scorer.compute(references=inf_references, predictions=inf_outputs)
-
+results = evaluate(references=references, predictions=outputs)
 print("Results on all squad:")
 print(results)
-print("Results on 500 squad:")
-print(inf_results)
 
 if args.output_file:
     with open(args.output_file, 'w') as f:
         for sample in outputs:
             f.write(json.dumps(sample) + '\n')
 if args.metrics_file:
-    results_dict = {
-        'f1': results['f1'],
-        'inf_f1': inf_results['f1'],
-    }
+    results_dict = results
     with open(args.metrics_file, 'w') as f:
         f.write(json.dumps(results_dict))
