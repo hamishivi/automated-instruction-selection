ea5c0df78d9a684429b38eb313011a4bf8c76b7e
Hamish Ivison <hamishivi@gmail.com>
2024-11-29 15:33:57 -0800

    add weka scripts

---
+++ shell_scripts/core_commands/model_training/weka/model_size_runs.sh
@@ -0,0 +1,19 @@
+FOLDERS=(
+    rds_100k_exp_from_base_weighted_mean_pool_meta-llama-Llama-2-7b-hf
+    rds_100k_exp_from_base_weighted_mean_pool_meta-llama-Llama-2-13b-hf
+    rds_100k_exp_from_base_weighted_mean_pool_meta-llama-Llama-2-70b-hf
+    rds_100k_exp_from_base_weighted_mean_pool_meta-llama-Llama-3.1-8B
+    rds_100k_exp_from_base_weighted_mean_pool_meta-llama-Llama-3.1-70B
+    rds_100k_exp_from_base_weighted_mean_pool_allenai-OLMo-2-1124-7B
+    rds_100k_exp_from_base_weighted_mean_pool_allenai-OLMo-2-1124-13B
+)
+
+for folder in ${FOLDERS[@]}; do
+    filepath=/weka/oe-adapt-default/hamishi/influence-tuning/${folder}/multitask_rrmax_10k.json
+    ./shell_scripts/core_commands/model_training/weka/full_finetune.sh $filepath training_7b_${folder}
+    ./shell_scripts/core_commands/model_training/weka/full_finetune_13b.sh $filepath training_13b_${folder}
+    ./shell_scripts/core_commands/model_training/weka/full_finetune_llama_3_8b.sh $filepath training_llama_3_8b_${folder}
+    ./shell_scripts/core_commands/model_training/weka/full_finetune_olmo_2_7b.sh $filepath training_olmo_2_7b_${folder}
+    ./shell_scripts/core_commands/model_training/weka/full_finetune_olmo_2_13b.sh $filepath training_olmo_2_13b_${folder}
+done
+./shell_scripts/core_commands/model_training/weka/full_finetune.sh
